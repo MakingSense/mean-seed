56904212cf95b6718e5f419d42722be68e3f0e4e
Mariano Ravinale <mravinale@gmail.com>
2014-03-21 19:58:57 -0300

    removed grunt minification

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,245 +14,20 @@ module.exports = function (grunt) {
   grunt.initConfig({
     yeoman: {
       // configurable paths
-      app: require('./bower.json').publicPath || 'public',
-      dist: 'staging',
-      views: 'views'
+      app: require('./bower.json').publicPath || 'public'
     },
       bower: {
           install: {
               //just run 'grunt bower:install' and you'll see files from your Bower packages in lib directory
           }
       },
-    express: {
-        options: {
-            port: process.env.PORT || 9000
-        },
-        dev: {
-            options: {
-                script: 'server.js'
-            }
-        },
-        staging: {
-            options: {
-                script: 'server.js',
-                node_env: 'staging'
-            }
-        },
-        prod: {
-            options: {
-                script: 'server.js',
-                node_env: 'production'
-            }
-        }
-    },
-    open: {
-      server: {
-        url: 'http://localhost:<%= express.options.port %>'
-      }
-    },
-    watch: {
-
-      express: {
-          files: [
-              '<%= yeoman.app %>/<%= yeoman.views %>/{,*//*}*.html',
-              '{.tmp,<%= yeoman.app %>}/styles/{,*//*}*.css',
-              '{.tmp,<%= yeoman.app %>}/scripts/{,*//*}*.js',
-              '<%= yeoman.app %>/images/{,*//*}*.{png,jpg,jpeg,gif,webp,svg}',
-              'server.js',
-              'api/{,*//*}*.{js,json}'
-          ],
-          tasks: ['express:dev'],
-          options: {
-              livereload: true,
-              nospawn: true //Without this option specified express won't be reloaded
-          }
-      },
-      styles: {
-        files: ['<%= yeoman.app %>/styles/{,*/}*.css'],
-        tasks: ['copy:styles', 'autoprefixer']
-      },
-      gruntfile: {
-        files: ['Gruntfile.js']
-      }
-    },
-    autoprefixer: {
-      options: ['last 1 version'],
-      dist: {
-        files: [{
-          expand: true,
-          cwd: '.tmp/styles/',
-          src: '{,*/}*.css',
-          dest: '.tmp/styles/'
-        }]
-      }
-    },
-    clean: {
-      dist: {
-        files: [{
-          dot: true,
-          src: [
-            '.tmp',
-            '<%= yeoman.dist %>',
-            '!<%= yeoman.dist %>/.git*'
-          ]
-        }]
-      },
-      heroku: {
-        files: [{
-          dot: true,
-          src: [
-            'public/scripts/',
-            'public/styles/',
-            'heroku/*',
-            '!heroku/.git*',
-            '!heroku/Procfile'                 
-          ]
-        }]
-      }, 
-      server: '.tmp'
-    },
     jshint: {
       options: {
         jshintrc: '.jshintrc',
         reporter: require('jshint-stylish')
       },
       all: [
-       // 'Gruntfile.js',
         '<%= yeoman.app %>/scripts/{,*/}*.js'
-      ]
-    },
-    rev: {
-      dist: {
-        files: {
-          src: [
-            '<%= yeoman.dist %>/scripts/{,*/}*.js',
-            '<%= yeoman.dist %>/styles/{,*/}*.css',
-            '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
-            '<%= yeoman.dist %>/styles/fonts/*'
-          ]
-        }
-      }
-    },
-    useminPrepare: {
-      html: '<%= yeoman.app %>/<%= yeoman.views %>/index.html',
-      options: {
-        dest: '<%= yeoman.dist %>'
-      }
-    },
-    usemin: {
-      html: ['<%= yeoman.dist %>/<%= yeoman.views %>/{,*/}*.html'],
-      css: ['<%= yeoman.dist %>/styles/{,*/}*.css'],
-      options: {
-        assetsDirs: ['<%= yeoman.dist %>']
-      }
-    },
-    imagemin: {
-      dist: {
-        files: [{
-          expand: true,
-          cwd: '<%= yeoman.app %>/images',
-          src: '{,*/}*.{png,jpg,jpeg}',
-          dest: '<%= yeoman.dist %>/images'
-        }]
-      }
-    },
-    svgmin: {
-      dist: {
-        files: [{
-          expand: true,
-          cwd: '<%= yeoman.app %>/images',
-          src: '{,*/}*.svg',
-          dest: '<%= yeoman.dist %>/images'
-        }]
-      }
-    },
-    cssmin: {
-      // By default, your `index.html` <!-- Usemin Block --> will take care of
-      // minification. This option is pre-configured if you do not wish to use
-      // Usemin blocks.
-      // dist: {
-      //   files: {
-      //     '<%= yeoman.dist %>/styles/main.css': [
-      //       '.tmp/styles/{,*/}*.css',
-      //       '<%= yeoman.app %>/styles/{,*/}*.css'
-      //     ]
-      //   }
-      // }
-    },
-    htmlmin: {
-      dist: {
-        options: {
-          /*removeCommentsFromCDATA: true,
-          // https://github.com/yeoman/grunt-usemin/issues/44
-          //collapseWhitespace: true,
-          collapseBooleanAttributes: true,
-          removeAttributeQuotes: true,
-          removeRedundantAttributes: true,
-          useShortDoctype: true,
-          removeEmptyAttributes: true,
-          removeOptionalTags: true*/
-        },
-        files: [{
-          expand: true,
-          cwd: '<%= yeoman.app %>/<%= yeoman.views %>',
-          src: ['*.html', 'partials/{,*/}*.html'],
-          dest: '<%= yeoman.dist %>/<%= yeoman.views %>'
-        }]
-      }
-    },
-    // Put files not handled in other tasks here
-    copy: {
-      dist: {
-        files: [{
-          expand: true,
-          dot: true,
-          cwd: '<%= yeoman.app %>',
-          dest: '<%= yeoman.dist %>',
-          src: [
-            '*.{ico,png,txt}',
-            '.htaccess',
-            'lib/**/*',
-            'images/{,*/}*.{gif,webp}',
-            'fonts/*'
-          ]
-        }, {
-          expand: true,
-          cwd: '.tmp/images',
-          dest: '<%= yeoman.dist %>/images',
-          src: [
-            'generated/*'
-          ]
-        }]
-      },
-      heroku: {
-        files: [{
-          expand: true,
-          dot: true,
-          cwd: '<%= yeoman.dist %>/',
-          dest: 'public/',
-          src: [  '**' ]
-        } ]
-      },  
-      styles: {
-        expand: true,
-        cwd: '<%= yeoman.app %>/styles',
-        dest: '.tmp/styles/',
-        src: '{,*/}*.css'
-      }
-    },
-    concurrent: {
-      server: [
-        'copy:styles'
-
-      ],
-      test: [
-        'copy:styles'
-      ],
-      dist: [
-        'copy:styles',
-        'imagemin',
-        'svgmin',
-        'htmlmin'
       ]
     },
     karma: {
@@ -260,95 +35,21 @@ module.exports = function (grunt) {
         configFile: 'test/karma.conf.js',
         singleRun: true
       }
-    },
-    cdnify: {
-      dist: {
-        html: ['<%= yeoman.dist %>/<%= yeoman.views %>/*.html']
-      }
-    },
-    concat: {
-          dist: {
-              src: ['<%= yeoman.app %>/scripts/**/*.js', '<%= yeoman.app %>/scripts/*.js'],
-              dest: '.tmp/concat/scripts/scripts.js'
-          }
-    },
-    ngmin: {
-      dist: {
-        files: [{
-          expand: true,
-          cwd: '.tmp/concat/scripts',
-          src: '*.js',
-          dest: '.tmp/concat/scripts'
-        }]
-      }
-    },
-    uglify: {
-      dist: {
-        files: {
-          '<%= yeoman.dist %>/scripts/scripts.js': [
-            '<%= yeoman.dist %>/scripts/scripts.js'
-          ]
-        }
-      }
-    }
-  });
-
-  grunt.registerTask('express-keepalive', 'Keep grunt running', function() {
-    this.async();
-  });
-
-  grunt.registerTask('server', function (target) {
-    if (target === 'staging') {
-      return grunt.task.run(['build', 'express:staging', 'open', 'express-keepalive']);
     }
 
-    if (target === 'ci') {
-      return grunt.task.run(['build','test']);
-    }
-
-    grunt.task.run([
-      'clean:server',
-      'concurrent:server',
-      'autoprefixer',
-      'express:dev',
-      'open',
-      'watch'
-    ]);
   });
+
 
   grunt.registerTask('test', [
-    'clean:server',
-    'concurrent:test',
-    'autoprefixer',
     'karma'
   ]);
 
   grunt.registerTask('build', [
-    'bower',
-    'clean:dist',
-    'useminPrepare',
-    'concurrent:dist',
-    'autoprefixer',
-    'concat',
-    'ngmin',
-    'copy:dist',
-    'cdnify',
-    'cssmin',
-    'uglify',
-    'rev',
-    'usemin',
-    'clean:server'
-  ]);
-
-  grunt.registerTask('heroku:production', [
-    'build',
-    'clean:heroku',
-    'copy:heroku',
-    'clean:dist'
+    'bower'
   ]);
 
   grunt.registerTask('default', [
-    'jshint',
+  //  'jshint',
     'test',
     'build'
   ]);

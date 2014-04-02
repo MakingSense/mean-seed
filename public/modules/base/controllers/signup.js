bcc8886343bdf1463d9368d8b6ab5f622bcf9c6d
Mariano Ravinale <mravinale@gmail.com>
2014-04-01 21:43:01 -0300

    started refactoring for oauth and user services

--- public/modules/base/controllers/signup.js
+++ public/modules/base/controllers/signup.js
@@ -1,9 +1,22 @@
 'use strict';
 
-angular.module('meanp').controller('SignupCtrl', function ($scope, Auth, $location) {
+angular.module('meanp').controller('SignupCtrl', function ($scope,$rootScope, Auth, $location, userService) {
 
 		$scope.register = function(form) {
+            $scope.errors = {};
 
+            userService.create($scope.user)
+                .success(function (response, status, headers, config) {
+                    $rootScope.currentUser = response;
+                    $location.path('/');
+                })
+                .error(function(response, status, headers, config) {
+                    angular.forEach(response.errors, function(error, field) {
+                        form[field].$setValidity('mongoose', false);
+                        $scope.errors[field] = error.type;
+                    });
+                });
+/*
 			Auth.createUser( $scope.user , function(err) {
 			  $scope.errors = {};
 
@@ -16,6 +29,6 @@ angular.module('meanp').controller('SignupCtrl', function ($scope, Auth, $locati
 				});
 			  }
 			});
-
+*/
 	    };
 });
--- public/modules/base/services/User.js
+++ public/modules/base/services/User.js
@@ -9,3 +9,27 @@ angular.module('meanp')
         }
       });
   });
+
+/* Services */
+angular.module('meanp').service('userService', function ($http) {
+
+    this.getAll = function () {
+        return $http.get('/auth/users/');
+    }
+
+    this.getById = function (blogItemId) {
+        return $http.get('/auth/users/'+blogItemId);
+    };
+
+    this.create = function (postData) {
+        return $http.post('/auth/users', postData);
+    };
+
+    this.update = function (blogItemId, blogItem) {
+        return $http.put('/auth/users/'+blogItemId, blogItem);
+    };
+
+    this.remove = function (blogItemId) {
+        return $http.delete('/auth/users/'+blogItemId);
+    };
+});

954f26aa977760eaa10bf39541d07ac2110cd367
Fearful <joan.lascano@gmail.com>
2014-07-16 11:12:36 -0300

    Quick fix to array

--- public/modules/app.js
+++ public/modules/app.js
@@ -35,7 +35,7 @@ angular.module('meanp', [
     $rootScope.$watch('currentUser', function(currentUser) {
       // if no currentUser and on a page that requires authorization then try to update it
       // will trigger 401s if user does not have a valid session
-      if (!currentUser && (['/login', '/logout', '/signup'].indexOf($location.path()) == -1 )) {
+      if (!currentUser && ['/login', '/logout', '/signup'].indexOf($location.path()) == -1) {
 
           sessionService.getCurrentUser()
               .success(function (response, status, headers, config) {

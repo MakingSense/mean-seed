3cd258a153e6abac3a0ef28a33f26ba5aaf16595
martinkeimel <martinkeimel@gmail.com>
2015-07-02 09:39:40 -0300

    Modifications to User signup

--- public/modules/base/controllers/signup.js
+++ public/modules/base/controllers/signup.js
@@ -1,8 +1,18 @@
 'use strict';
 
-angular.module('mean').controller('SignupCtrl', function ($scope, $rootScope, $location, userService, authService) {
+angular.module('mean').controller('SignupCtrl', function ($scope, $rootScope, $location, userService, authService, roleService) {
 
         $scope.errorMessage = '';
+        $scope.roles = [];
+        $scope.user = {};
+        
+        roleService.getAll()
+         .then(function (response, status, headers, config) {
+                  $scope.roles = response.data;
+                  $scope.user.role = $scope.roles[0];
+                }, function (response, status, headers, config) {
+                      $scope.errorMessage = response.data.message;
+                });
 
 		$scope.register = function(form) {
             

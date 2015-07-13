9c27011f373657556f34e18dc6143ac8b8a66476
matiasbeckerle <matiasbeckerle@gmail.com>
2015-07-13 16:14:53 -0300

    Improve grunt tasks using clean, copy, concat and usemin
    - This also adds two more tasks: for builds and to simulate production environment.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,13 @@
 'use strict';
 
 // TODO: create a task for UI development to improve their process.
-// TODO: create a task for building.
 // TODO: use concurrent where it fits.
 
 module.exports = function (grunt) {
 
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-uglify');
@@ -13,6 +15,7 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-open');
     grunt.loadNpmTasks('grunt-sass');
     grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-usemin');
     grunt.loadNpmTasks('grunt-karma');
     grunt.loadNpmTasks('grunt-mocha-test');
     grunt.loadNpmTasks('grunt-protractor-runner');
@@ -28,10 +31,38 @@ module.exports = function (grunt) {
             api: './api',
             app: './public',
             dist: './public/dist',
+            images: './public/images',
+            lib: './public/lib',
             modules: './public/modules',
-            assets: './public/assets',
             styles: './public/styles',
             test: './test'
+        },
+
+        /**
+         * Cleans up "dist" directory before building.
+         * Relies on: grunt-contrib-clean.
+         */
+        clean: {
+            dist: {
+                src: ['<%= paths.dist %>']
+            }
+        },
+
+        /**
+         * Copies static files to "dist" directory. Used for building purposes.
+         * Relies on: grunt-contrib-copy.
+         */
+        copy: {
+            dist: {
+                files: [
+                    { expand: true, cwd: '<%= paths.images %>/', src: ['**'], dest: '<%= paths.dist %>/images/' },
+                    { expand: true, cwd: '<%= paths.modules %>/', src: ['**/views/**/*'], dest: '<%= paths.dist %>/modules/' },
+                    { src: '<%= paths.app %>/.htaccess', dest: '<%= paths.dist %>/.htaccess' },
+                    { src: '<%= paths.app %>/favicon.ico', dest: '<%= paths.dist %>/favicon.ico' },
+                    { src: '<%= paths.app %>/index.html', dest: '<%= paths.dist %>/index.html' },
+                    { src: '<%= paths.app %>/robots.txt', dest: '<%= paths.dist %>/robots.txt' }
+                ]
+            }
         },
         
         /**
@@ -55,15 +86,6 @@ module.exports = function (grunt) {
          * Relies on: grunt-contrib-uglify.
          */
         uglify: {
-            dev: {
-                options: {
-                    mangle: false,
-                    sourceMap: true,
-                },
-                files: {
-                    '<%= paths.dist %>/scripts/all.min.js': ['<%= paths.modules %>/**/*.js']
-                }
-            },
             dist: {
                 options: {
                     mangle: false,
@@ -73,13 +95,76 @@ module.exports = function (grunt) {
                     }
                 },
                 files: {
-                    '<%= paths.dist %>/scripts/all.min.js': ['<%= paths.modules %>/**/*.js']
+                    '<%= paths.dist %>/scripts/lib.js': ['<%= paths.dist %>/scripts/lib.js'],
+                    '<%= paths.dist %>/scripts/app.js': ['<%= paths.dist %>/scripts/app.js']
                 }
+            }
+        },
+
+        /**
+         * Updates the grunt configuration to apply a configured transformation flow to tagged files.
+         * Relies on: grunt-usemin.
+         */
+        useminPrepare: {
+            html: '<%= paths.dist %>/index.html',
+            options: {
+                dest: '<%= paths.dist %>'
+            }
+        },
+
+        /**
+         * First it replaces all the blocks with a single "summary" line, pointing to a file creating by the transformation flow.
+         * Then it looks for references to assets (i.e. images, scripts, ...), and tries to replace them.
+         * Relies on: grunt-usemin.
+         */
+        usemin: {
+            html: ['<%= paths.dist %>/index.html'],
+            options: {
+                dirs: ['<%= paths.dist %>']
+            }
+        },
+
+        /**
+         * Concatenates several files into a single one.
+         * Relies on: grunt-contrib-concat.
+         */
+        concat: {
+            styles: {
+                files: [
+                    {
+                        dest: '<%= paths.dist %>/styles/main.css',
+                        src: '<%= paths.styles %>/main.css'
+                    }
+                ]
+            },
+            scripts: {
+                files: [
+                    {
+                        dest: '<%= paths.dist %>/scripts/lib.js',
+                        src: [
+                            '<%= paths.lib %>/jquery/jquery.min.js',
+                            '<%= paths.lib %>/angular/angular.min.js',
+                            '<%= paths.lib %>/angular-resource/angular-resource.js',
+                            '<%= paths.lib %>/angular-cookies/angular-cookies.js',
+                            '<%= paths.lib %>/ngstorage/ngStorage.min.js',
+                            '<%= paths.lib %>/angular-sanitize/angular-sanitize.js',
+                            '<%= paths.lib %>/angular-route/angular-route.js',
+                            '<%= paths.lib %>/angular-http-auth/src/http-auth-interceptor.js',
+                            '<%= paths.lib %>/autofill-directive/autofill-directive.js',
+                            '<%= paths.lib %>/underscore/underscore-min.js'
+                        ]
+                    },
+                    {
+                        dest: '<%= paths.dist %>/scripts/app.js',
+                        src: ['<%= paths.modules %>/**/*.js']
+                    }
+                ]
             }
         },
         
         /**
          * Starts up the server on a predefined port.
+         * Relies on: grunt-express-server.
          */
         express: {
             options: {
@@ -89,11 +174,18 @@ module.exports = function (grunt) {
                 options: {
                     script: '<%= paths.root %>/server.js'
                 }
+            },
+            prod: {
+                options: {
+                    script: '<%= paths.root %>/server.js',
+                    node_env: 'production'
+                }
             }
         },
         
         /**
          * Opens the project in a new tab of your browser.
+         * Relies on: grunt-open.
          */
         open: {
             server: {
@@ -125,12 +217,6 @@ module.exports = function (grunt) {
                     '<%= paths.test %>/**/*.js',
                 ],
                 tasks: ['validate']
-            },
-            uglify: {
-                files: [
-                    '<%= paths.modules %>/**/*.js'
-                ],
-                tasks: ['uglify:dev']
             },
             sass: {
                 files: [
@@ -165,13 +251,14 @@ module.exports = function (grunt) {
             },
             dist: {
                 files: {
-                    '<%= paths.dist %>/styles/main.css': '<%= paths.styles %>/main.scss'
+                    '<%= paths.styles %>/main.css': '<%= paths.styles %>/main.scss'
                 }
             }
         },
         
         /**
          * Adds the vendor prefix for styles.
+         * Relies on: grunt-autoprefixer.
          */
         autoprefixer: {
             options: {
@@ -180,13 +267,14 @@ module.exports = function (grunt) {
             css: {
                 expand: true,
                 flatten: true,
-                src: '<%= paths.dist %>/styles/*.css',
-                dest: '<%= paths.dist %>/styles/'
+                src: '<%= paths.styles %>/*.css',
+                dest: '<%= paths.styles %>/'
             }
         },
         
         /**
          * Test runner on the front-end side.
+         * Relies on: grunt-karma.
          */
         karma: {
             unit: {
@@ -248,9 +336,33 @@ module.exports = function (grunt) {
      */
     grunt.registerTask('default', [
         'express:dev',
-        'uglify:dev',
         'sass',
         'autoprefixer',
+        'open',
+        'watch'
+    ]);
+
+    /**
+     * Builds the application and generates a bundle in "dist" folder.
+     */
+    grunt.registerTask('build', [
+        'clean:dist',
+        'copy:dist',
+        'useminPrepare',
+        'concat:scripts',
+        'uglify:dist',
+        'sass',
+        'autoprefixer',
+        'concat:styles',
+        'usemin'
+    ]);
+
+    /**
+     * A task to simulate production environment.
+     */
+    grunt.registerTask('production', [
+        'build',
+        'express:prod',
         'open',
         'watch'
     ]);

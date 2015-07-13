d97fa134aa8f7da0ffd8b35da3976a6b66538324
matiasbeckerle <matiasbeckerle@gmail.com>
2015-07-13 16:12:17 -0300

    Add env config

--- api/config/appConfig.js
+++ api/config/appConfig.js
@@ -1,14 +1,15 @@
 'use strict';
 
 module.exports = {
-  port: process.env.PORT || 3000,
-  db: {
-    uri: process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/meanp',
-    options: {
-      db: {
-        safe: true
-      }
-    }
-  },
-  secretKey: 'ourSecretKey'
+    env: process.env.node_env == 'production' ? 'production' : 'development',
+    port: process.env.PORT || 3000,
+    db: {
+        uri: process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/meanp',
+        options: {
+            db: {
+                safe: true
+            }
+        }
+    },
+    secretKey: 'ourSecretKey'
 };
